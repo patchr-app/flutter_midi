077d9775d23f609d04fb9dfba8fa3c418fec0a3a
Michael Marner <michael@20papercups.net>
2020-06-14 21:42:26 +0930

    WIP ios midi implementation

--- libs/midi/midi/ios/Classes/Constants.swift
+++ libs/midi/midi/ios/Classes/Constants.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Flutter
+
+public class Constants {
+  public static var METHOD_CHANNEL_NAME = "com.synthfeeder/midi";
+  public static var DEVICE_CHANNEL_NAME = "com.synthfeeder/midi/devices";
+  public static var MESSAGE_CHANNEL_NAME = "com.synthfeeder/midi/messages";
+  
+  
+  public static var GET_INPUTS = "getInputs";
+  public static var GET_OUTPUTS = "getOutputs";
+  
+  public static var OPEN_INPUT = "openInput";
+  public static var OPEN_OUTPUT = "openOutput";
+  
+  public static var CLOSE_INPUT = "closeInput";
+  public static var CLOSE_OUTPUT = "closeOutput";
+  
+  public static var SEND = "send";
+  
+  public static var ID = "id";
+  public static var MANUFACTURER = "manufacturer";
+  public static var NAME = "name";
+  public static var VERSION = "version";
+  
+  
+  public static var ERR_NOT_OPEN = "404";
+  public static var ERR_IO = "500";
+  
+  
+  public static var PORT = "port";
+  public static var DATA = "data";
+  
+  public static var INPUT = "i";
+  public static var OUTPUT = "o";
+  public static var NUMBER = "number";
+  
+  public static var TYPE = "type";
+  
+  public static var STATE = "state";
+  public static var CONNECTED = "connected";
+  public static var DISCONNECTED = "disconnected";
+}
+
+
